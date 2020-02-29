373ec7651af4fe2e7aa5702f687c94e306c408b6
Yoshiyuki Hirano <yhirano@me.com>
2020-02-29 20:15:38 +0900

    Add deleting hook to Author

--- app/Author.php
+++ app/Author.php
@@ -3,9 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Author extends Model
 {
+    protected static function boot()
+    {
+      parent::boot();
+
+      static::deleting(function ($author) {
+        Storage::disk('public')->delete($author->image_path);
+      });
+    }
+
     protected $fillable = [
       'first_name',
       'last_name',
